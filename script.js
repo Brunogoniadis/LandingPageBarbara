62d59d2ba027644e47c9b722cd7d4f24408ecde9
Bruno Goniadis <56612714+Brunogoniadis@users.noreply.github.com>
2022-09-06 00:19:57 -0300

    Commit inicial
    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const menu = document.querySelector('.menu');
+const NavMenu = document.querySelector('.nav-menu');
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+menu.addEventListener('click', ()=> {
+    menu.classList.toggle('ativo');
+    NavMenu.classList.toggle('ativo');
+})
